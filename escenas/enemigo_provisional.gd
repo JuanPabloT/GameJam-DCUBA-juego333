d21295cf789a4e30e37bfeafebc23d7ae982324a
JuanPabloT <juanpitarela@gmail.com>
2026-03-08 13:44:11 -0300

    moveset de enemigos

--- escenas/enemigo_provisional.gd
+++ escenas/enemigo_provisional.gd
@@ -64,49 +64,103 @@ func on_turn():
 			enemy.deal_ordinary_damage(randi_range(15, 0))
 
 func _clemen_tina_turn() -> void:
-	
+	match randi_range(0,2):
+		0:
+			self.add_shield(5)
+			enemy.apply_water()
+			enemy.deal_ordinary_damage(randi_range(3, 6))
+		1:
+			self.heal_by(7)
+			enemy.apply_lightning()
+			enemy.deal_ordinary_damage(randi_range(3, 6))
+		2:
+			enemy.set_on_fire()
+			enemy.apply_poison(2)
+			enemy.deal_ordinary_damage(randi_range(4, 10))
 	pass
 
 
 func _elemental_turn() -> void:
-	#ataques que aplican fuego
-	pass
+	match randi_range(0,2):
+		0:
+			enemy.set_on_fire()
+			enemy.deal_ordinary_damage(randi_range(2, 7))	
+		1:
+			enemy.set_on_fire()
+			enemy.set_on_fire()
+		2:
+			self.set_on_fire()
+			self.heal_by(10)
+			enemy.deal_ordinary_damage(randi_range(0, 5))
 
 
 func _liliana_turn() -> void:
-	#ataques que aplican viento
-	pass
-
+	match randi_range(0,6):
+		0,1,2:
+			enemy.apply_wind()
+			enemy.deal_ordinary_damage(randi_range(4, 8))
+		3,4:
+			enemy.apply_wind()
+			self.apply_wind()
+		5,6:
+			self.set_on_fire()
+			self.apply_wind()
+			enemy.deal_ordinary_damage(5)
+			
 
 func _gaucho_turn() -> void:
-	pass
+	match randi_range(0,5):
+		0,1,2:
+			self.apply_beer()
+			enemy.deal_ordinary_damage(randi_range(4, 7))
+		3,4:
+			self.apply_beer()
+			self.apply_beer()
+			enemy.deal_ordinary_damage(randi_range(3, 6))
+		5:
+			enemy.apply_beer()
+			self.apply_water()
+			enemy.deal_ordinary_damage(randi_range(4, 10))
 
 
 func _alien_turn() -> void:
 	match randi_range(0,3):
 		0:
+			enemy.apply_lightning()
 			self.add_shield(10)
 		1,2:
 			self.add_shield(5)
-			enemy.deal_ordinary_damage(randi_range(5, 10))
+			enemy.deal_ordinary_damage(randi_range(3, 8))
 		3:
-			enemy.deal_ordinary_damage(randi_range(5, 15))
+			self.apply_wind()
+			enemy.deal_ordinary_damage(randi_range(4, 10))
 
 
 func _bruja_turn() -> void:
-	#ataques que aplican agua
 	match randi_range(0,2):
 		0:
-			pass
+			enemy.apply_poison(2)
+			self.heal_by(5)
 		1:
-			pass
+			self.apply_water()
+			self.heal_by(8)
+			enemy.deal_ordinary_damage(randi_range(1, 6))
 		2:
-			enemy.deal_ordinary_damage(randi_range(5, 15))
+			enemy.apply_poison(2)
+			enemy.deal_ordinary_damage(randi_range(5, 10))
 
 
 func _apple_turn() -> void:
-	#ataques que aplican root
-	pass
+	match randi_range(0,4):
+		0,1:
+			enemy.apply_root()
+			self.apply_root()
+			enemy.deal_ordinary_damage(randi_range(1, 6))
+		2,3:
+			enemy.apply_root()
+			enemy.deal_ordinary_damage(randi_range(4, 8))
+		4:
+			enemy.deal_ordinary_damage(randi_range(10, 20))
 
 
 func _on_beer_pressed() -> void:
