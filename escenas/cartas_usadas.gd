5a436b7106174fc7463d2b3df1746878700fe9f5
JuanPabloT <juanpitarela@gmail.com>
2026-03-09 05:06:44 -0300

    texto para cartas

--- escenas/cartas_usadas.gd
+++ escenas/cartas_usadas.gd
@@ -13,3 +13,27 @@ func _process(delta: float) -> void:
 
 func _on_iniciar_pressed() -> void:
 	get_tree().change_scene_to_file("res://escenas/mainmenu.tscn")
+
+
+func _on_areabinbario_mouse_entered() -> void:
+	$"Camera2D/Main Menu/binario/Tooltip".visible = true
+
+
+func _on_areabinbario_mouse_exited() -> void:
+	$"Camera2D/Main Menu/binario/Tooltip".visible = false
+
+
+func _on_areaamargo_y_retruco_mouse_entered() -> void:
+	$"Camera2D/Main Menu/amargo_y_retruco/Tooltip".visible = true
+
+
+func _on_areaamargo_y_retruco_mouse_exited() -> void:
+	$"Camera2D/Main Menu/amargo_y_retruco/Tooltip".visible = false
+
+
+func _on_areaacapella_mouse_entered() -> void:
+	$"Camera2D/Main Menu/acapella/Tooltip".visible = true
+
+
+func _on_areaacapella_mouse_exited() -> void:
+	$"Camera2D/Main Menu/acapella/Tooltip".visible = false
